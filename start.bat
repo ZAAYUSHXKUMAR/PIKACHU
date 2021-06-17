97d8f0cd1891d360f9955a109004d70ca167d4cb
ZAAYUSHXKUMAR <85151817+ZAAYUSHXKUMAR@users.noreply.github.com>
2021-06-17 16:16:13 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Saitama Robot
+TITLE REBORN-PIKACHU
 :: Enables virtual env mode and then starts saitama
-env\scripts\activate.bat && py -m SaitamaRobot
+env\scripts\activate.bat && py -m REBORN-PIKACHU
