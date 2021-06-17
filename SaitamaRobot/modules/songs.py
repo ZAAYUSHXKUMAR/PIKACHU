35fdb39eb30010c88e043a5aac268633a0c10b9b
ZAAYUSHXKUMAR <ZAAYUSHXKUMAR@users.noreply.github.com>
2021-06-17 10:21:17 +0000

    Replaced new name

--- SaitamaRobot/modules/songs.py
+++ SaitamaRobot/modules/songs.py
@@ -5,9 +5,9 @@ import aiohttp
 import youtube_dl
 
 from pyrogram import filters
-from SaitamaRobot import pgram
+from PIKACHU import pgram
 from youtube_search import YoutubeSearch
-from SaitamaRobot.pyrogramee.errors import capture_err
+from PIKACHU.pyrogramee.errors import capture_err
 
 
 def time_to_seconds(time):
